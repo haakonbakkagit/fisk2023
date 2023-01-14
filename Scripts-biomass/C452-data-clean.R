f02327d5e0d06ebe21b06dd9539453273cf413a3
Haakon Bakka <46645905+haakonbakkagit@users.noreply.github.com>
2023-01-14 12:51:33 +0100

    combine tables and set up integration

--- Scripts-biomass/C452-data-clean.R
+++ Scripts-biomass/C452-data-clean.R
@@ -16,16 +16,31 @@ df2=read.xlsx(file1, sheet=2)
 names(df1)[2]
 names(df1)[2] = "Std.sigma"
 
+## These numbers are meant to be exactly round
+df1$Average.weight = round(df1$Average.weight, digits = 1)
+
 ## Clean df2 ----
 
 names(df2)[1]
 names(df2)[1] = "Month.name"
 
-## Transform df2 ----
+### Transform df2 ----
 df2$Month.ind = 1:12
 df2$Biom.per.ind = df2$Biomass/df2$Number.of.individuals
 
-## Save data
+## Round the number, standard rounding rules
+df2$Biom.p.i.rnd = round(df2$Biom.per.ind, digits = 1)
+
+## Find matching stdev
+## Where in df1 do we find the correct stdev?
+which.row = match(x = df2$Biom.p.i.rnd, table = df1$Average.weight)
+## Use that
+df2$Biom.sd.from.df1 = df1$Std.sigma[which.row]
+
+## Later: Possible improvement: Weighted average of the two nearby values
+
+
+### Save data ----
 folder = "../Results-biomass/"
 dir.create(folder, showWarnings = FALSE)
 
@@ -35,4 +50,4 @@ write.xlsx(df1, file = paste0(folder, filename))
 filename="C452-df2-transf.xlsx"
 write.xlsx(df2, file = paste0(folder, filename))
 
-
+### END ----
--- Scripts-biomass/C453-model1.R
+++ Scripts-biomass/C453-model1.R
@@ -11,7 +11,7 @@ file1 = "../Results-biomass/C452-df1-transf.xlsx"
 if(!file.exists(file1)) stop("Please run 452 first!")
 df1=read.xlsx(file1, sheet=1)
 file2 = "../Results-biomass/C452-df2-transf.xlsx"
-df2=read.xlsx(file2, sheet=2)
+df2=read.xlsx(file2, sheet=1)
 
 ### INPUT Constants
 # Feel free to change these and rerun the code
@@ -21,9 +21,36 @@ harvest.cutoff.kg = 4
 ## Question 1 ----
 # total harvestable biomass (fish larger than 4kg) for these months?
 
+## Decision: Use the correct mean instead of the rounded
+## Percentage above cutoff value
+perc.above.cut0 = 1 - pnorm(harvest.cutoff.kg, mean=df2$Biom.per.ind, 
+                               sd = df2$Biom.sd.from.df1)
+
+## Local functions where value is picked dynamically from local environment
+local.dnorm = function(x) dnorm(x, mean, sd)
+local.xdnorm = function(x) x*dnorm(x, mean, sd)
+
+df2$perc.above.cut = NA
+
+for (i.row in 1:nrow(df2)) {
+  mean = df2$Biom.per.ind[i.row]
+  sd = df2$Biom.sd.from.df1[i.row]
+  ## compute the integral
+  intgr = integrate(local.dnorm, lower=harvest.cutoff.kg, upper=mean+10*sd)
+  stopifnot(intgr$abs.error < 0.01)
+  df2$perc.above.cut[i.row] = intgr$value
+}
+
+if (F) {
+  ## Test
+  df2$perc.above.cut
+  ## should equal
+  perc.above.cut0
+}
 
 
-## Question 1 ----
+
+## Question 2 ----
 # What is the average weight of these (harvestable) fish?
 
 
