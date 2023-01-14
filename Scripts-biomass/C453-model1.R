02328e6ee830edd08d953ed7aceab9d479fd4bd3
Haakon Bakka <46645905+haakonbakkagit@users.noreply.github.com>
2023-01-14 13:17:03 +0100

    Update C453-model1.R

--- Scripts-biomass/C453-model1.R
+++ Scripts-biomass/C453-model1.R
@@ -17,8 +17,12 @@ df2=read.xlsx(file2, sheet=1)
 # Feel free to change these and rerun the code
 harvest.cutoff.kg = 4
 # - Recommended value: 4
+## How many (n) discretisation bins in the simulation problem?
+n.sim.disc.bins = 1000
+# - Higher -> more accurate
+# - Lower -> faster
 
-## Question 1 ----
+## Question 0 and 1 ----
 # total harvestable biomass (fish larger than 4kg) for these months?
 
 ## Decision: Use the correct mean instead of the rounded
@@ -56,7 +60,7 @@ df2$exp.biom/df2$perc.above.cut
 df2$exp.biom*df2$Number.of.individuals
 
 ## Percent of biomass that will be harvested
-df2$exp.biom*df2$Number.of.individuals/df2$Biomass
+round(df2$exp.biom*df2$Number.of.individuals/df2$Biomass, 2)*100
 
 
 
@@ -70,9 +74,61 @@ if (F) {
 
 
 ## Question 2 ----
-# What is the average weight of these (harvestable) fish?
+# Assuming you only know the biomass and number of individuals at the start of 
+## the first month (from Table 2). Assume a growth rate of 11,2%. How much will 
+## be harvested during the next 12 months, if we assume that all fish over 4kg 
+## will be harvested at the end of each month?
+
+## Values of initial time point
+mean = df2$Biom.per.ind[1]
+sd = df2$Biom.sd.from.df1[1]
+
+## x values
+kg.vals = seq(mean-5*sd, mean+5*sd, length.out=n.sim.disc.bins)
+
+## Discrete probability distribution
+d.discrete = dnorm(kg.vals, mean, sd=sd)
+d.discrete = d.discrete/sum(d.discrete)
+
+## Individuals, not rounded
+## Important to not round, otherwise approx becomes bad
+individuals = d.discrete*df2$Number.of.individuals[1]
+
+if (F) {
+  ## check
+  plot(kg.vals, d.discrete)
+  plot(kg.vals, individuals)
+}
+
+## MAYBE: DO two full matrices, for easy summarisation
+
+
+## Initial month
+sim = cbind(kg.vals, ind.m1.start = individuals)
+df3 = data.frame(month=0, ind.start = sum(sim$ind.m1.start))
+## Note: df3 will not be consistent with df1!
+
+## Add all the other months
+for (i.m in 1:12) {
+  ## i.m is the month index
+  
+  ## Near end of this month
+  ## Do growth (not slaughter)
+  
+  ## Beginning of next month: 
+  ## Do slaughter
+  
+}
+
+
+local.addmonth = function(sim) {
+  ## This function adds one month to the sim dataframe
+  
+}
 
 
 
 
+
+### END ----
 
