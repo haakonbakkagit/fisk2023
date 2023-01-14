de6536254e45bc2043bd03721f478a740fe10a52
Haakon Bakka <46645905+haakonbakkagit@users.noreply.github.com>
2023-01-14 15:15:22 +0100

    Simplify logic

--- Scripts-biomass/C453-model1.R
+++ Scripts-biomass/C453-model1.R
@@ -22,8 +22,8 @@ n.sim.disc.bins = 1000
 # - Higher -> more accurate
 # - Lower -> faster
 ## How fast do the fisg grow in the simulation?
-sim.growth.factor = 0.112
-# - Recommended: 11.2% -> 0.112
+sim.growth.factor = 1.112
+# - Recommended: 11.2% -> factor is 1.112
 
 ## Question 0 and 1 ----
 # total harvestable biomass (fish larger than 4kg) for these months?
@@ -45,11 +45,11 @@ for (i.row in 1:nrow(df2)) {
   sd = df2$Biom.sd.from.df1[i.row]
   ## Compute the integral to get probability of picking 1 slaughter ind
   intgr1 = integrate(local.dnorm, lower=harvest.cutoff.kg, upper=mean+10*sd)
-  stopifnot(intgr$abs.error < 0.01)
+  stopifnot(intgr1$abs.error < 0.01)
   df2$perc.above.cut[i.row] = intgr1$value
   ## Integral for expected slaughter biomass for 1 pick
   intgr2 = integrate(local.xdnorm, lower=harvest.cutoff.kg, upper=mean+10*sd)
-  stopifnot(intgr$abs.error < 1)
+  stopifnot(intgr2$abs.error < 1)
   df2$exp.biom[i.row] = intgr2$value
 }
 
@@ -93,10 +93,12 @@ mean = df2$Biom.per.ind[1]
 sd = df2$Biom.sd.from.df1[1]
 
 ## x values
-kg.vals = seq(mean-5*sd, mean+5*sd, length.out=n.sim.disc.bins)
+sim.kg.start = seq(0, mean+5*sd, length.out=n.sim.disc.bins)
+## remove 0 kg category:
+sim.kg.start = sim.kg.start[-1]
 
 ## Discrete probability distribution
-d.discrete = dnorm(kg.vals, mean, sd=sd)
+d.discrete = dnorm(sim.kg.start, mean, sd=sd)
 d.discrete = d.discrete/sum(d.discrete)
 
 ## Individuals, not rounded
@@ -105,63 +107,66 @@ individuals = d.discrete*df2$Number.of.individuals[1]
 
 if (F) {
   ## check
-  plot(kg.vals, d.discrete)
-  plot(kg.vals, individuals)
+  plot(sim.kg.start, d.discrete)
+  plot(sim.kg.start, individuals)
 }
 
 sim.individuals = individuals
 
 ## Create a summary dataframe with results
-sim.summary = data.frame(month=1:12, 
-                         individuals=NA, 
+summ = data.frame(month=1:12, 
+                         individ.start=NA, 
                          biomass.start=NA, biomass.growth = NA,
                          harvest.ind=NA, harvest.biom=NA)
-sim.summary$individuals[1] = df2$Number.of.individuals[1]
+summ$individ.start[1] = df2$Number.of.individuals[1]
 
 ## Create two full matrices, for easy summarisation later
 ## Represent dead individuals as 0 kg
 
 ## Simulation number of individuals at start of month 
 ## (after slaughter)
-sim.kg.start = matrix(0, ncol=12+1, nrow=length(sim.individuals))
-sim.kg.start[, 1] = kg.vals
+#sim.kg.start = matrix(0, ncol=12+1, nrow=length(sim.individuals))
+#sim.kg.start[, 1] = sim.kg.start
 ## Simulation number of individuals near the end of month
 ## (just before slaughter)
-sim.kg.ne = matrix(0, ncol=12, nrow=length(sim.individuals))
+#sim.kg.ne = matrix(0, ncol=12, nrow=length(sim.individuals))
 
+## Start with all fish being alive
+is.alive = rep(T, length=length(sim.individuals))
+
+stop ( "here")
 
 ## Add all the other months
 for (i.m in 1:12) {
   ## i.m is the month index
   
   ## Beginning of this month
-  is.alive = (sim.kg.start[, i.m]>0.00001)
-  sim.summary$individuals[i.m] = sum(sim.individuals[is.alive])
-  sim.summary$biomass.start[i.m] = mean(sim.kg.start[, i.m]*sim.individuals*is.alive)
+  ## is.alive are alive
+  summ$individ.start[i.m] = sum(sim.individuals[is.alive])
   
   ## Near end of this month
-  ## Do growth (not slaughter)
-  sim.summary$biomass.growth[i.m] = sim.summary$biomass.start*sim.growth.factor
-  sim.kg.ne[, i.m] = sim.kg.start[, i.m]*sim.growth.factor
+  ## Do growth, before harvest/slaughter
+  pre.harvest.kg = sim.kg.start*sim.growth.factor^i.m
+  
+  ## Harvest
+  is.this.harvest = (pre.harvest.kg > harvest.cutoff.kg) & is.alive
+  num.xgrid = sum(is.this.harvest)
+  
+  summ$harvest.ind[i.m] = sum(sim.individuals[is.this.harvest])
+  
+  summ$harvest.biom[i.m] = sum(
+    pre.harvest.kg[is.this.harvest]*sim.individuals[is.this.harvest])
   
   ## Beginning of next month: 
-  ## Do slaughter
-  sim.kg.start[, i.m+1] = sim.kg.ne[,i.m]
-  is.harvest = (sim.kg.start[, i.m+1]>= harvest.cutoff.kg)
-  ## Here, slaughtered individuals are represented as 0 kg
-  sim.kg.start[, i.m+1][is.harvest] = 0
+  ## After harvest
+  is.alive = (pre.harvest.kg <= harvest.cutoff.kg)
   
-  ## How much biomass got slaughtered?
-  sim.summary$harvest.biom[i.m] = sum(sim.kg.ne[, i.m][is.harvest]*
-    sim.individuals[is.harvest])
   
   
 }
 
-sim.summary
+summ
 
-sim.kg.start[1:5, 1:5]
-sim.kg.ne[1:5, 1:5]
 
 
 ### Improvements ----
